d1e028ea96d3070b4d0bf10b653d0d3984872079
Ankur <34418414+Ankuraxz@users.noreply.github.com>
2020-09-06 20:11:25 +0530

    Update ldr_led_lcd.ino

--- Experiment 2/LDR + LCD/ldr_led_lcd.ino
+++ Experiment 2/LDR + LCD/ldr_led_lcd.ino
@@ -5,13 +5,14 @@ decreases resistance with respect to receiving
 luminosity on the component's sensitive surface. 
 */
 int value=0;
+int lumen =0;
 #include <LiquidCrystal.h>
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 6, 5, 4, 3);
 void setup()
 {
   pinMode(A0, INPUT);
-  pinMode(13, OUTPUT);
+  pinMode(9, OUTPUT);
   Serial.begin(9600);
   
   lcd.begin(16, 2);
@@ -24,9 +25,10 @@ void loop()
 {
   lcd.setCursor(0, 1);
   value= analogRead(A0);
+  lumen = 255- (map(value,1,290,0,255));
   if(value<30)
   {
-    digitalWrite(13, HIGH); 
+    analogWrite(9, lumen); 
     Serial.println("Light ON");
     Serial.println(value);
     lcd.clear();
@@ -36,7 +38,7 @@ void loop()
   }
   else
   {
-     digitalWrite(13, LOW);
+    analogWrite(9, lumen); 
     Serial.println("Light OFF");
     Serial.println(value);
     lcd.clear();
