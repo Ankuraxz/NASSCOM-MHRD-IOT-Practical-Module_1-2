c52c8bd2945af9a2ca372a01939cd5d36026abe2
Rajat Bansal <65078224+rajatbansal01@users.noreply.github.com>
2020-09-09 18:18:36 +0530

    Create fade.ino

---
+++ Submissions/Rajat_Bansal/Practical1/fade.ino
@@ -0,0 +1,42 @@
+int brightness = 0;
+
+void setup()
+{
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  for (brightness = 0; brightness <= 255; brightness += 5) 
+  {
+    analogWrite(11, brightness);
+    delay(30); 
+  }
+  for (brightness = 255; brightness >= 0; brightness -= 5) 
+  {
+    analogWrite(11, brightness);
+    delay(30); 
+  }
+    for (brightness = 0; brightness <= 255; brightness += 5) 
+  {
+    analogWrite(10, brightness);
+    delay(30); 
+  }
+  for (brightness = 255; brightness >= 0; brightness -= 5) 
+  {
+    analogWrite(10, brightness);
+    delay(30); 
+  }
+    for (brightness = 0; brightness <= 255; brightness += 5) 
+  {
+    analogWrite(9, brightness);
+    delay(30); 
+  }
+  for (brightness = 255; brightness >= 0; brightness -= 5) 
+  {
+    analogWrite(9, brightness);
+    delay(30); 
+  }
+}
