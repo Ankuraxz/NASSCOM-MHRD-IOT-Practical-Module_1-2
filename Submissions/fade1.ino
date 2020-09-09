d9bf2eed18a127d6d3cedaa62e0067adcf0ecc30
tanishkbatheja22 <67550749+tanishkbatheja22@users.noreply.github.com>
2020-09-09 18:29:40 +0530

    Create fade1.ino

---
+++ Submissions/fade1.ino
@@ -0,0 +1,24 @@
+
+int ledPin = 9;    // LED connected to digital pin 9
+
+void setup() {
+  // nothing happens in setup
+}
+
+void loop() {
+  // fade in from min to max in increments of 5 points:
+  for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+
+  // fade out from max to min in increments of 5 points:
+  for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin, fadeValue);
+    // wait for 30 milliseconds to see the dimming effect
+    delay(30);
+  }
+}
